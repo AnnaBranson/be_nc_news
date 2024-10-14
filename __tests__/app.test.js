7f13ab7e488cd1fa82ab03eb9bda1141288a806c
Anna Branson <annabranson@Annas-MacBook-Pro.local>
2024-10-14 15:54:55 +0100

    completed get reqest for article by article ID. responds with obj containing correct keys and handles status codes 200, 400 and 404 (using psql code)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -58,5 +58,49 @@ describe("api/topics ", () => {
 
 })
 
+describe("api/articles/:article_id ", () => {
+    test("GET: 200 response with the requested article object", () => {
+        return request(app)
+        .get("/api/articles/3")
+        .expect(200)
+        .then(({ body }) => {
+           expect(body.article).toMatchObject({
+            article_id: expect.any(Number),
+            title: expect.any(String),  
+            author: expect.any(String),  
+            body: expect.any(String),     
+            topic: expect.any(String),    
+            created_at: expect.any(String), 
+            votes: expect.any(Number),   
+            article_img_url: expect.any(String) 
+           })
+        })
+    });
+    test("GET: 400 ", () => {
+        return request(app)
+        .get("/api/articles/not-a-number")
+        .expect(400)
+        .then(({ body: { msg } }) => {
+            expect(msg).toBe("Bad Request!")          
+        })
+    })
+    test("GET: 404 response with an error message if passed a valid article_id that does not exist in the database ", () => {
+        return request(app)
+        .get("/api/articles/9999")
+        .expect(404)
+        .then(({ body: { msg } }) => {
+            expect(msg).toBe("Not Found!")          
+        })
+    })
+   
+
+})
+
+
+
+      
+
+
+
 
       
